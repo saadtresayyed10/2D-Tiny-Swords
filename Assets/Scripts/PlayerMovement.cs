30fd467ab69d1b2698febaa926d076cf81a1d37f
Saad Sayyed <saadsyed950@gmail.com>
2025-08-28 00:05:27 +0530

    Animation of walking and idle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
   public float speed = 5;
     public Rigidbody2D rb;
+    public Animator anim;
 
     // Fixed Update is called 50x frame
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
 
         rb.velocity = new Vector2(horizontal, vertical) * speed;
 
