091ebe994a77b13e690ee7c365dcafe0cca96f1e
Saad Sayyed <saadsyed950@gmail.com>
2025-08-28 00:09:45 +0530

    Character flipping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  public float speed = 5;
+    public float speed = 5;
     public Rigidbody2D rb;
     public Animator anim;
+
+    public int facingDirection = 1;
 
     // Fixed Update is called 50x frame
     void FixedUpdate()
@@ -14,10 +16,22 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        if(horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
         anim.SetFloat("horizontal", Mathf.Abs(horizontal));
         anim.SetFloat("vertical", Mathf.Abs(vertical));
 
         rb.velocity = new Vector2(horizontal, vertical) * speed;
 
     }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
 }
